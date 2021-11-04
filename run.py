535314058d6bb2e7282e7d5729bb6e239db58a5e
Martin Bannister <martin.bannister91@gmail.com>
2021-11-04 12:45:06 +0000

    TIDY(run.py): Change name of variable in update_surplus_worksheet function

--- run.py
+++ run.py
@@ -68,8 +68,8 @@ def update_surplus_worksheet(data):
     Update surplus worksheet, add new row with the list data provided.
     """
     print('Updating surplus worksheet...\n')
-    sales_worksheet = SHEET.worksheet('surplus')
-    sales_worksheet.append_row(data)
+    surplus_worksheet = SHEET.worksheet('surplus')
+    surplus_worksheet.append_row(data)
     print('Surplus worksheet updated successfully.\n')
 
 
